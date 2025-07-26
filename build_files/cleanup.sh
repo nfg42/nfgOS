ec145b36ae9228f34b993bdb74d21e865ece1076
nfg42 <3077570+nfg42@users.noreply.github.com>
2025-07-26 13:55:21 -0500

    Update cleanup.sh

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -10,7 +10,7 @@ log() {
 log "Starting system cleanup"
 
 # Remove autostart files
-rm /etc/skel/.config/autostart/steam.desktop
+#rm /etc/skel/.config/autostart/steam.desktop
 
 # Clean package manager cache
 dnf5 clean all
