8a69a08aa14fbdbbd046c382c2d08bebdb97f4ad
nfg42 <3077570+nfg42@users.noreply.github.com>
2025-07-26 13:48:32 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -87,13 +87,13 @@ install -m 0755 "$CLI_DIR/cursor" /usr/bin/cursor-cli
 #log "Adding Amy OS just recipes"
 #echo "import \"/usr/share/amyos/just/amy.just\"" >>/usr/share/ublue-os/justfile
 
-log "Hide incompatible Bazzite just recipes"
-for recipe in "install-coolercontrol" "install-openrgb"; do
-  if ! grep -l "^$recipe:" /usr/share/ublue-os/just/*.just | grep -q .; then
-    echo "Error: Recipe $recipe not found in any just file"
-    exit 1
-  fi
-  sed -i "s/^$recipe:/_$recipe:/" /usr/share/ublue-os/just/*.just
-done
+#log "Hide incompatible Bazzite just recipes"
+#for recipe in "install-coolercontrol" "install-openrgb"; do
+#  if ! grep -l "^$recipe:" /usr/share/ublue-os/just/*.just | grep -q .; then
+#    echo "Error: Recipe $recipe not found in any just file"
+#    exit 1
+#  fi
+#  sed -i "s/^$recipe:/_$recipe:/" /usr/share/ublue-os/just/*.just
+#done
 
 log "Build process completed"
